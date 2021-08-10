c6359f5276266eb1c2c0fc1546f6da180dc7164a
mintduck <yjshin@bitnine.net>
2021-08-11 04:17:43 +0900

    upload
    4.5 Cutting the summary

--- src/Movie.js
+++ src/Movie.js
@@ -15,7 +15,7 @@ function Movie({ year, title, summary, poster, genres }) {
               {genre}</li>
           ))}
         </ul>
-        <p className="movie__summary">{summary}</p>
+        <p className="movie__summary">{summary.slice(0, 140)}...</p>
       </div>
     </div>
   );
