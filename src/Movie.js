f7cc740da4080d3a7d6d4ea36307769e8e539bd4
mintduck <yjshin@bitnine.net>
2021-08-11 03:05:20 +0900

    updaate
    4.3 Adding Genres

--- src/Movie.js
+++ src/Movie.js
@@ -4,14 +4,20 @@ import "./Movie.css";
 
 //movie component는 stat가 필요없음 function 컴포넌트로 만들자.
 
-function Movie({ year, title, summary, poster }) {
+function Movie({ year, title, summary, poster, genres }) {
   return (
-    <div class="movies__movie">
+    <div className="movies__movie">
       <img src={poster} alt={title} title={title} />
-      <div class="movie__data">
-        <h3 class="movie_title">{title}</h3>
-        <h5 class="movie__year">{year}</h5>
-        <p class="movie__summary">{summary}</p>
+      <div className="movie__data">
+        <h3 className="movie_title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="genres__genre">
+              {genre}</li>
+          ))}
+        </ul>
+        <p className="movie__summary">{summary}</p>
       </div>
     </div>
   );
@@ -22,7 +28,8 @@ Movie.propTypes = {
   year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
-  poster: PropTypes.string.isRequired
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired
 }
 
 export default Movie;
