e50d440966d2527683182bcbd4436d24d20b34e8
mintduck <77373079+mintduck@users.noreply.github.com>
2021-08-11 13:22:47 +0900

    update
    6.4 Redirecting

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,10 +1,26 @@
 import React from "react";
 
-function Detail(props) {
-    console.log(props);
-    return(
-        <span>hello</span>
-    );
+class Detail extends React.Component {
+  componentDidMount() {
+    console.log(this.props);
+    const { location, history } = this.props;
+    //console.log(location.state);
+
+    //state가 undefined 일 때(카드를 클릭하지 않고, url에 직접 movie-detail을 입력한 경우 state가 undefined 이다.)
+    //home으로 리다이렉트 해주자.(movie-detail은 클릭으로만 접근 가능)
+    if (location.state === undefined) {
+      history.push("/");
+    }
+  }
+  render() {
+    const { location } = this.props;
+    if (location.state) {
+      return <span>{location.state.title}</span>;
+    } else {
+      return null;
+    }
+
+  }
 }
 
 export default Detail;
