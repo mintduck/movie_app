dd081102bf87e4838f8681b88235e072aa893569
mintduck <77373079+mintduck@users.noreply.github.com>
2021-08-11 10:53:26 +0900

    Revert "upload"
    This reverts commit 86084d1773733ec9694726edc2d3069629564292.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,12 @@ import React from 'react';
 import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
-import Navigation from './components/Navigation';
-import "./App.css";
 
 function App() {
   return (
     <HashRouter>
-      {/* <Link>는 router 밖에서 사용할 수 없다. 따라서 <Navigation>도 지금처럼 Router 안에 존재해야 한다. */}
-      <Navigation />
       <Route path="/" exact={true} component={Home} />
+      {/* path about.js path로 가면 About 컴포넌트를 보여줘라 */}
       <Route path="/about" component={About} />
     </HashRouter>
   );
