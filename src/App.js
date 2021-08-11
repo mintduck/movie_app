9ead7e8278ce8d0d27a7d34d30bede9d3f1ef85c
mintduck <77373079+mintduck@users.noreply.github.com>
2021-08-11 10:26:06 +0900

    update
    6.1 Building the Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
 
-function App(){
-  return <span>something</span>;
+function App() {
+  return (
+    <HashRouter>
+      <Route path="/" exact={true} component={Home} />
+      {/* path about.js path로 가면 About 컴포넌트를 보여줘라 */}
+      <Route path="/about" component={About} />
+    </HashRouter>
+  );
 }
 
 export default App;
