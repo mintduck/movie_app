bc2b77280099dd82f2d00e759bb761be685d0418
mintduck <77373079+mintduck@users.noreply.github.com>
2021-08-11 12:52:14 +0900

    update
    6.3 Sharing Props Between Routes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React from 'react';
 import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
+import Detail from "./routes/Detail";
 import Navigation from './components/Navigation';
 import "./App.css";
 
 function App() {
   return (
+    // 라우터에 있는 모든 라우트는 props를 기본값으로 갖고있다.
     <HashRouter>
-      {/* <Link>는 router 밖에서 사용할 수 없다. 따라서 <Navigation>도 지금처럼 Router 안에 존재해야 한다. */}
       <Navigation />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
+      <Route path="/movie-detail" component={Detail} />
     </HashRouter>
   );
 }
--- src/routes/About.js
+++ src/routes/About.js
@@ -1,7 +1,10 @@
 import React from "react";
 import "./About.css";
 
-function About() {
+function About(props) {
+  // 라우터에 있는 모든 라우트는 props를 기본값으로 갖고있다.
+  //console.log를 통해 어떤 prop를 갖고 있는지 확인해보자
+  console.log(props);
     return (
         <div className="about__container">
           <span>
